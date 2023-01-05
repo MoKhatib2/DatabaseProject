04651dd4be06b5e9a2b7923e1a9fbd3ad3701af0
MoKhatib2 <121944981+MoKhatib2@users.noreply.github.com>
2023-01-05 20:49:00 +0100

    Add files via upload

---
+++ SAM_Register.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SAM_Register.aspx.cs" Inherits="WebApp.Sports_Association_Manager_Register" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Title" runat="server" Text="Sports Assossiation Manager" style="font-weight: 700"></asp:Label>
+            <br />
+            <br />
+            Name:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="Name" runat="server"  Width="163px"></asp:TextBox>
+        </div>
+        <p>
+            Username:&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="Username" runat="server"  Width="163px"></asp:TextBox>
+        </p>
+        <p>
+            Password:&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="Password" runat="server"  Width="163px"></asp:TextBox>
+        </p>
+&nbsp;<asp:Button ID="SAM_RegisterBUTTON" runat="server" Text="Register" Width="105px" OnClick="SAM_RegisterBUTTON_Click" />
+        <br />
+        <br />
+        <asp:Button ID="BackBUTTON" runat="server" Text="Back" OnClick="BackBUTTON_Click" />
+    </form>
+</body>
+</html>
---
+++ SAM_Register.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace WebApp
+{
+    public partial class Sports_Association_Manager_Register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SAM_RegisterBUTTON_Click(object sender, EventArgs e)
+        {
+            String connStr = WebConfigurationManager.ConnectionStrings["WebApp"].ToString();
+            SqlConnection conn = new SqlConnection(connStr);
+            DataTable dt = new DataTable();
+            DataRow dr;
+
+            String username = Username.Text;
+            String pass = Password.Text;
+            String name = Name.Text;
+        
+
+            bool usernameTaken = false;
+            SqlDataAdapter da = new SqlDataAdapter("select*from SystemUser", conn);
+            da.Fill(dt);
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                dr = dt.Rows[i];
+                if (dr[0].Equals(username))
+                {
+                    Response.Write("username already taken!");
+                    usernameTaken = true;
+                }
+            }
+
+            if (!usernameTaken)
+            {
+                SqlCommand addSam = new SqlCommand("addAssociationManager", conn);
+                addSam.CommandType = CommandType.StoredProcedure;
+
+                addSam.Parameters.Add(new SqlParameter("@username", username));
+                addSam.Parameters.Add(new SqlParameter("@password", pass));
+                addSam.Parameters.Add(new SqlParameter("@name", name));
+
+                conn.Open();
+                addSam.ExecuteNonQuery();
+                conn.Close();
+
+                Response.Write("Account Created");
+
+            }
+        }
+
+        protected void BackBUTTON_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("MainRegister.aspx");
+        }
+    }
+}
