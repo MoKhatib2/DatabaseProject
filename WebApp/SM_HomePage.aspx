224505b9bd167a98c97bad02894eff6a1ce4e22d
MoKhatib2 <121944981+MoKhatib2@users.noreply.github.com>
2023-01-05 23:25:56 +0100

    Add files via upload
    SM Home page updated

---
+++ WebApp/SM_HomePage.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SM_HomePage.aspx.cs" Inherits="WebApp.SM_HomePage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Club being Managed"></asp:Label>
+        </div>
+        <asp:Table ID="ManagedStadium" runat="server" Height="62px" Width="89px">
+        </asp:Table>
+        <asp:Label ID="Label2" runat="server" Text="Received Requests"></asp:Label>
+        <br />
+        <asp:Table ID="ReceivedRequests" runat="server" Height="58px" Width="66px">
+        </asp:Table>
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="Unhandeled Requests"></asp:Label>
+        <br />
+        <asp:Table ID="UnhandeledRequests" runat="server" Height="78px" Width="94px">
+        </asp:Table>
+        <br />
+            <asp:Label ID="Label3" runat="server" Text="Please enter match id"></asp:Label>
+        <br />
+        <br />
+        <asp:TextBox ID="AcceptOrRejectID" runat="server"></asp:TextBox>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="AcceptBUTTON" runat="server" Text="Accept Request" OnClick="AcceptBUTTON_Click" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="RejectBUTTON" runat="server" Text="Reject Request" OnClick="RejectBUTTON_Click" />
+        &nbsp;
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ WebApp/SM_HomePage.aspx.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace WebApp
+{
+    public partial class SM_HomePage : System.Web.UI.Page
+    {
+
+        private void fillUITable(Table uitable, DataTable dataTable)
+        {
+            for (int j = 0; j < dataTable.Columns.Count; j++)
+            {
+                TableHeaderCell headerCell = new TableHeaderCell();
+                headerCell.Text = dataTable.Columns[j].ColumnName;
+                uitable.Rows.Add(new TableRow());
+                uitable.Rows[0].Cells.Add(headerCell);
+                uitable.Rows[0].Cells[j].BorderStyle = BorderStyle.Solid;
+            }
+
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                uitable.Rows.Add(new TableRow());
+                for (int j = 0; j < dataTable.Columns.Count; j++)
+                {
+                    TableCell cell = new TableCell();
+                    cell.Text = dataTable.Rows[i][j].ToString();
+                    uitable.Rows[i + 1].Cells.Add(cell);
+                    uitable.Rows[i + 1].Cells[j].BorderStyle = BorderStyle.Solid;
+
+                }
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["WebApp"].ToString());
+            DataTable table = new DataTable();
+
+            SqlDataAdapter adapter = new SqlDataAdapter("select cr.rname as ClubRepresentative, host.cname as HostClub, guest.cname as GuestClub, m.startTime as StartTime, m.endTime as EndTime, h.approved\r\n        from HostRequest h\r\n        inner join StadiumManager sm on h.stadiumManagerID = sm.id\r\n        inner join ClubRepresentative cr on h.clubRepresentativeID = cr.id\r\n        inner join Match m on h.matchID = m.id\r\n        inner join Club guest on guest.id = m.guestClubID\r\n    inner join Club host on host.id = m.hostClubID  where sm.username = '" + Session["User"] +"'", conn);
+            adapter.Fill(table);
+            fillUITable(ReceivedRequests, table);
+
+            table = new DataTable();
+            adapter = new SqlDataAdapter("select s.id as StadiumID, s.sname as StadiumName, s.available as Available, s.capacity as Capacity, s.slocation as Location from stadium s inner join StadiumManager sm on s.stadiumManagerID = sm.id where sm.username = '" + Session["User"] + "'", conn);
+            adapter.Fill(table);
+            fillUITable(ManagedStadium, table);
+
+            table = new DataTable();
+            adapter = new SqlDataAdapter("select m.id as MatchID, cr.rname as ClubRepresentative, host.cname as HostClub, guest.cname as GuestClub, m.startTime as StartTime, m.endTime as EndTime \r\n        from HostRequest h\r\n        inner join StadiumManager sm on h.stadiumManagerID = sm.id\r\n        inner join ClubRepresentative cr on h.clubRepresentativeID = cr.id\r\n        inner join Match m on h.matchID = m.id\r\n        inner join Club guest on guest.id = m.guestClubID\r\n    inner join Club host on host.id = m.hostClubID  where h.approved is null and sm.username = '" + Session["User"] + "'", conn);
+            adapter.Fill(table);
+            fillUITable(UnhandeledRequests, table);
+
+        }
+
+        protected void AcceptBUTTON_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["WebApp"].ToString());
+
+            var StadiumManager ="";
+            var HostClub ="";
+            var GuestClub ="";
+            var StartTime="";
+            var MatchID = AcceptOrRejectID.Text;
+
+            DataTable table = new DataTable();
+            DataRow dr;
+            SqlDataAdapter adapter = new SqlDataAdapter("select sm.username as StadiumManager, host.cname as HostClub, guest.cname as GuestClub, m.startTime as StartTime\r\n        from HostRequest h\r\n        inner join StadiumManager sm on h.stadiumManagerID = sm.id\r\n        inner join ClubRepresentative cr on h.clubRepresentativeID = cr.id\r\n        inner join Match m on h.matchID = m.id\r\n        inner join Club guest on guest.id = m.guestClubID\r\n    inner join Club host on host.id = m.hostClubID  where m.id = '" + MatchID + "' and  sm.username = '" + Session["User"] + "'", conn);
+
+            adapter.Fill(table);
+            for(int i = 0; i< table.Rows.Count; i++) 
+            {
+                 dr = table.Rows[i];
+                 StadiumManager = Convert.ToString(dr[0]);
+                 HostClub = Convert.ToString(dr[1]);
+                 GuestClub = Convert.ToString(dr[2]);
+                 StartTime = Convert.ToString(dr[3]);
+            
+            }
+
+            if (table.Rows.Count > 0)
+            {
+                SqlCommand proc = new SqlCommand("acceptRequest", conn);
+                proc.CommandType = System.Data.CommandType.StoredProcedure;
+                proc.Parameters.Add(new SqlParameter("@stadMUsername", StadiumManager));
+                proc.Parameters.Add(new SqlParameter("@hostClub", HostClub));
+                proc.Parameters.Add(new SqlParameter("@guestClub", GuestClub));
+                proc.Parameters.Add(new SqlParameter("@StartTime", StartTime));
+
+                Response.Write("Success?");
+                conn.Open();
+                proc.ExecuteNonQuery();
+                conn.Close();
+            }
+        
+        }
+
+        protected void RejectBUTTON_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["WebApp"].ToString());
+
+            var StadiumManager = "";
+            var HostClub = "";
+            var GuestClub = "";
+            var StartTime = "";
+            var MatchID = AcceptOrRejectID.Text;
+
+            DataTable table = new DataTable();
+            DataRow dr;
+            SqlDataAdapter adapter = new SqlDataAdapter("select sm.username as StadiumManager, host.cname as HostClub, guest.cname as GuestClub, m.startTime as StartTime\r\n        from HostRequest h\r\n        inner join StadiumManager sm on h.stadiumManagerID = sm.id\r\n        inner join ClubRepresentative cr on h.clubRepresentativeID = cr.id\r\n        inner join Match m on h.matchID = m.id\r\n        inner join Club guest on guest.id = m.guestClubID\r\n    inner join Club host on host.id = m.hostClubID  where m.id = '" + MatchID + "' and  sm.username = '" + Session["User"] + "'", conn);
+
+            adapter.Fill(table);
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+                dr = table.Rows[i];
+                StadiumManager = Convert.ToString(dr[0]);
+                HostClub = Convert.ToString(dr[1]);
+                GuestClub = Convert.ToString(dr[2]);
+                StartTime = Convert.ToString(dr[3]);
+
+            }
+
+            if (table.Rows.Count > 0)
+            {
+                SqlCommand proc = new SqlCommand("RejectRequest", conn);
+                proc.CommandType = System.Data.CommandType.StoredProcedure;
+                proc.Parameters.Add(new SqlParameter("@stadMUsername", StadiumManager));
+                proc.Parameters.Add(new SqlParameter("@hostClub", HostClub));
+                proc.Parameters.Add(new SqlParameter("@guestClub", GuestClub));
+                proc.Parameters.Add(new SqlParameter("@StartTime", StartTime));
+
+                Response.Write("Success?");
+                conn.Open();
+                proc.ExecuteNonQuery();
+                conn.Close();
+            }
+
+        }
+    }
+}
