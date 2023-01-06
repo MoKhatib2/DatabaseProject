c6780beed14b40de20798bc9e80c24a4519e6ed4
MoKhatib2 <121944981+MoKhatib2@users.noreply.github.com>
2023-01-06 23:10:59 +0100

    updated Fan HomePage

---
+++ WebApp/Fan_HomePage.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Fan_HomePage.aspx.cs" Inherits="WebApp.Fan1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Please enter the time to view the matches with available tickets starting from this time "></asp:Label>
+            <br />
+            <br />
+            &nbsp;<asp:TextBox ID="StartTime" runat="server" style="margin-bottom: 0px"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="ViewMatchesBUTTON" runat="server" Text="View Matches" OnClick="ViewMatchesBUTTON_Click" />
+            &nbsp;&nbsp;&nbsp;
+            <br />
+            <br />
+        </div>
+        <p>
+            <asp:Table ID="AvailableMatches" runat="server" Height="46px" Width="61px">
+            </asp:Table>
+        </p>
+        <p>
+            &nbsp;</p>
+          <asp:GridView ID="AvailableTickets" runat="server" CssClass="table"
+            width="50%" AutoGenerateColumns="False" DataKeyNames="MatchID"  >
+            <Columns>
+                <asp:BoundField DataField="HostClub" HeaderText="HostClub"       />
+                <asp:BoundField DataField="GuestClub" HeaderText="GuestClub"     />
+                <asp:BoundField DataField="StartTime" HeaderText="StartTime"               />
+                <asp:TemplateField HeaderText = "GetTicket" ItemStyle-HorizontalAlign ="Center" >
+                    <ItemTemplate>
+                    <asp:Button ID="Purchase" runat="server" Text="Purchase" 
+                       CssClass="btn" 
+                        OnClick="PurchaseBUTTON_Click"
+                     />
+                    </ItemTemplate>
+                </asp:TemplateField>
+                 
+            </Columns>
+        </asp:GridView>
+        <p>
+                <asp:Button ID="BackBUTTON" runat="server" style="margin-right: 0px" Text="Back" Width="90px" OnClick="BackBUTTON_Click" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebApp/Fan_HomePage.aspx.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp
+{
+    public partial class Fan1 : System.Web.UI.Page
+    {
+        private void fillUITable(Table uitable, DataTable dataTable)
+        {
+            for (int j = 0; j < dataTable.Columns.Count; j++)
+            {
+                TableHeaderCell headerCell = new TableHeaderCell();
+                headerCell.Text = dataTable.Columns[j].ColumnName;
+                uitable.Rows.Add(new TableRow());
+                uitable.Rows[0].Cells.Add(headerCell);
+                uitable.Rows[0].Cells[j].BorderStyle = BorderStyle.Solid;
+            }
+
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                uitable.Rows.Add(new TableRow());
+                for (int j = 0; j < dataTable.Columns.Count; j++)
+                {
+                    TableCell cell = new TableCell();
+                    cell.Text = dataTable.Rows[i][j].ToString();
+                    uitable.Rows[i + 1].Cells.Add(cell);
+                    uitable.Rows[i + 1].Cells[j].BorderStyle = BorderStyle.Solid;
+
+                }
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                LoadGrid();
+        }
+        void LoadGrid()
+        {
+            using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["WebApp"].ToString()))
+            {
+                String strSQL = "select m.id as MatchID , host.cname as HostClub, guest.cname as GuestClub, m.StartTime as StartTime \r\n\t\tfrom Match m\r\n\t\tinner join Club host on m.hostClubID = host.id\r\n\t\tinner join Club guest on m.guestClubID = guest.id\r\n\t\tinner join Stadium s on m.stadiumID = s.id\r\n\t\twhere exists (select * from Ticket t where t.matchID = m.id and t.available = 1) and m.startTime >= CURRENT_TIMESTAMP";
+                using (SqlCommand cmdSQL = new SqlCommand(strSQL, conn))
+                {
+                    DataTable rstData = new DataTable();
+                    conn.Open();
+                    rstData.Load(cmdSQL.ExecuteReader());
+                    AvailableTickets.DataSource = rstData;
+                    AvailableTickets.DataBind();
+                }
+            }
+        }
+      
+
+        protected void ViewMatchesBUTTON_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["WebApp"].ToString());
+            DataTable table = new DataTable();
+
+            var startTime = StartTime.Text;
+
+            SqlDataAdapter adapter = new SqlDataAdapter("select host.cname as HostClub, guest.cname as GuestClub, m.StartTime as StartTime \r\n\t\tfrom Match m\r\n\t\tinner join Club host on m.hostClubID = host.id\r\n\t\tinner join Club guest on m.guestClubID = guest.id\r\n\t\tinner join Stadium s on m.stadiumID = s.id\r\n\t\twhere exists (select * from Ticket t where t.matchID = m.id and t.available = 1) and m.startTime >= '" + startTime + "'", conn);
+            adapter.Fill(table);
+            fillUITable(AvailableMatches, table);
+        }
+        protected void PurchaseBUTTON_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["WebApp"].ToString());
+            Button btn = sender as Button;
+            GridViewRow gRow = btn.NamingContainer as GridViewRow;
+            var hostClub = Convert.ToString(gRow.Cells[0].Text);
+            var guestClub = Convert.ToString(gRow.Cells[1].Text);
+            var startTime = DateTime.Parse(Convert.ToString(gRow.Cells[2].Text)).ToString("yyyy-MM-dd HH:mm:ss");
+            var nationalID = "";
+           
+
+            DataTable NationalID_table = new DataTable();
+            DataRow dr;
+            SqlDataAdapter adapter=  new SqlDataAdapter("select nationalID from Fan where username = '" + Session["User"] + "'", conn);
+
+            adapter.Fill(NationalID_table);
+            for (int i = 0; i < NationalID_table.Rows.Count; i++)
+            {
+                dr = NationalID_table.Rows[i];
+                nationalID = Convert.ToString(dr[0]);
+
+            }
+            Response.Write(nationalID);
+
+            SqlCommand proc = new SqlCommand("purchaseTicket", conn);
+            proc.CommandType = System.Data.CommandType.StoredProcedure;
+            proc.Parameters.Add(new SqlParameter("@nationalID", nationalID));
+            proc.Parameters.Add(new SqlParameter("@hostClub", hostClub));
+            proc.Parameters.Add(new SqlParameter("@guestClub", guestClub));
+            proc.Parameters.Add(new SqlParameter("@startTime", startTime));
+
+            conn.Open();
+            proc.ExecuteNonQuery();
+            conn.Close();
+
+            if (!IsPostBack)
+                LoadGrid();
+
+            Page.Response.Redirect(Page.Request.Url.ToString(), true);
+
+
+        }
+
+        protected void BackBUTTON_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("StartPage.aspx");
+        }
+    }
+ }
+
